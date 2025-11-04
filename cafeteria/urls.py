b4006792c91db1e474939be6c3c40d1f0a3e18d5
Enrique Alejandro Espinoza Rodriguez <enrique.alejandro3928@gmail.com>
2025-11-03 18:04:58 -0600

    Agregue paginas

--- cafeteria/urls.py
+++ cafeteria/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name="index"),
-    path('menu/',views.menu , name="menu")
+    path('', views.index, name='index'),
+    path('menu/', views.menu, name='menu'),
+    path('eventos/', views.eventos, name='eventos'),
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('tienda-online/', views.tienda_online, name='tienda_online'),
+    path('ubicacion/', views.ubicacion, name='ubicacion'),
 ]
--- cafeteria/views.py
+++ cafeteria/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render
 from . import models
 
 # Create your views here.
-def index (request):
+def index(request):
     return render(request, "index.html")
 
 def menu(request):
     datos = {
-        "info" : models.Producto.objects.all()
+        "info": models.Producto.objects.all()
     }
     return render(request, "menu.html", context=datos)
+
+def eventos(request):
+    return render(request, "eventos.html")
+
+def nosotros(request):
+    return render(request, "nosotros.html")
+
+def tienda_online(request):
+    return render(request, "tienda_online.html")
+
+def ubicacion(request):
+    return render(request, "ubicacion.html")
