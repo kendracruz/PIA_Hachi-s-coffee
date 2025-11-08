20654f17be430ee6e30b054bf3e5bf41b7fb2f97
Kendra Yvette Cruz Bruno <kendracrux@gmail.com>
2025-11-07 21:16:42 -0600

    agregue info

---
+++ pia/static/js/menu.js
@@ -0,0 +1,17 @@
+// menu.js
+document.addEventListener("DOMContentLoaded", () => {
+  const elements = document.querySelectorAll(".fade-in");
+
+  function checkVisible() {
+    const triggerBottom = window.innerHeight * 0.9;
+    elements.forEach(el => {
+      const top = el.getBoundingClientRect().top;
+      if (top < triggerBottom) {
+        el.classList.add("visible");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", checkVisible);
+  checkVisible();
+});
