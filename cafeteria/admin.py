b2dc6d022e90430f139e4c0c9fc2a9198b1914b3
Enrique Alejandro Espinoza Rodriguez <enrique.alejandro3928@gmail.com>
2025-11-07 21:17:50 -0600

    agregue info

--- cafeteria/admin.py
+++ cafeteria/admin.py
@@ -2,3 +2,7 @@ from django.contrib import admin
 from . import models 
 # Register your models here.
 admin.site.register(models.Producto)
+admin.site.register(models.Sucursal)
+admin.site.register(models.Auto)
+admin.site.register(models.Evento)
+admin.site.register(models.Comentario)
